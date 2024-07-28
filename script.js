a1bdd52b55c1e2b54a6d68eca95759a4f0f5a6e4
husam4610 <jackhawash1997@gmail.com>
2024-07-28 10:53:32 +0300

    gg

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    var menuLinks = document.querySelector('.menu-links');
+    menuLinks.classList.toggle('open');
+    var icons = document.querySelector('.hamburger-icon');
+    icons.classList.toggle('open');
+}
